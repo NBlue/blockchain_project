6e99e515d4faa2fa27d59372d34beefd5b19be34
Sotatek-NamNguyen14 <nam.nguyen14@sotatek.com>
2023-06-13 16:39:34 +0700

    Feat: base pj2

--- Project2/CS251_Dex_V2_Starter/contracts/token.sol
+++ Project2/CS251_Dex_V2_Starter/contracts/token.sol
@@ -4,37 +4,29 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
- 
 // Your token contract
 contract Token is Ownable, ERC20 {
-    string private constant _symbol = '';                 // TODO: Give your token a symbol (all caps!)
-    string private constant _name = '';                   // TODO: Give your token a name
+    string private constant _symbol = "DHN"; // TODO: Give your token a symbol (all caps!)
+    string private constant _name = "DuhanaToken"; // TODO: Give your token a name
+    bool private _mintingEnabled = true;
+
+    event MintingDisabled();
 
     constructor() ERC20(_name, _symbol) {}
-
-    // ============================================================
-    //                    FUNCTIONS TO IMPLEMENT
-    // ============================================================
 
     // Function _mint: Create more of your tokens.
     // You can change the inputs, or the scope of your function, as needed.
     // Do not remove the AdminOnly modifier!
-    function mint(uint amount) 
-        public 
-        onlyOwner
-    {
-        /******* TODO: Implement this function *******/
-
+    function mint(uint256 amount) public onlyOwner {
+        require(_mintingEnabled, "Minting is disabled");
+        _mint(msg.sender, amount);
     }
 
     // Function _disable_mint: Disable future minting of your token.
     // You can change the inputs, or the scope of your function, as needed.
     // Do not remove the AdminOnly modifier!
-    function disable_mint()
-        public
-        onlyOwner
-    {
-        /******* TODO: Implement this function *******/
-
+    function disable_mint() public onlyOwner {
+        _mintingEnabled = false;
+        emit MintingDisabled();
     }
 }
